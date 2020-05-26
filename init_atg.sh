a8a9becac4aae8e7391f4e74dcedb7e2a4b5f732
Zhang, JieZ <jiez.zhang@intel.com>
2020-05-26 15:28:45 +0800

    Add dpcpp-base for dpcpp generator

--- init_atg.sh
+++ init_atg.sh
@@ -8,12 +8,14 @@
 screen -t "RETESTER"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_core.py --retest; bash'  
 
 # Standart generators
-screen -t "fuzz"    sh -c '. . ./atg_profile &&. ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g fuzz; bash'
+#screen -t "fuzz"    sh -c '. . ./atg_profile &&. ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g fuzz; bash'
 
-screen -t "oldgen"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g oldgen; bash'
+#screen -t "oldgen"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g oldgen; bash'
 
-screen -t "base"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g base; bash'
+#screen -t "base"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g base; bash'
 
-screen -t "cexpr"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g cexpr; bash'
+#screen -t "cexpr"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g cexpr; bash'
 
-screen -t "fuzz"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g fuzz; bash'
+#screen -t "fuzz"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g fuzz; bash'
+
+screen -t "dpcpp-base"    sh -c '. ./atg_profile && . ./ATG/bin/activate &&  mkdir --parents $ATG_WORK_DIR; cd $ATG_WORK_DIR;  python $ATG_HOME_DIR/atg_test.py -c xmain -g dpcpp-base; bash'
